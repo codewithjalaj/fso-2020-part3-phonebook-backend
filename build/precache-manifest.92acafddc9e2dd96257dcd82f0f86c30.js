cfb00ef54eef79d3194d757d98409927686c52df
codewithjalaj <jalaj@mail.com>
2020-10-20 18:24:09 +0530

    added frontend to be served from backend

---
+++ build/precache-manifest.92acafddc9e2dd96257dcd82f0f86c30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4440ecfe9de811eb670af3d8e54a91d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e79ea7ba160c63507729",
+    "url": "/static/css/main.718b195c.chunk.css"
+  },
+  {
+    "revision": "05bfc191cfe6d41493d4",
+    "url": "/static/js/2.866bf98a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.866bf98a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e79ea7ba160c63507729",
+    "url": "/static/js/main.dd31f4c8.chunk.js"
+  },
+  {
+    "revision": "10b02f26725c68861e1b",
+    "url": "/static/js/runtime-main.ddc9bda2.js"
+  }
+]);
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ morgan.token('data', (req) => {
 	return JSON.stringify(req.body);
 });
 
+app.use(express.static('build'));
 app.use(cors());
 app.use(express.json());
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] - :data'));
